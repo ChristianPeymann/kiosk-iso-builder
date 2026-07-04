5373cb832b46e1b194e85c173763ba0a514a4c55
Christian Peymann <christian.peymann@ewe.net>
2026-07-04 19:16:08 +0200

    Fix profiledef and permissions

--- profiledef.sh
+++ profiledef.sh
@@ -8,3 +8,13 @@ install_dir="arch"
 buildmodes=('iso')
 bootmodes=('uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="erofs"
+airootfs_image_tool_options=('-zlz4hc,12' -E ztailpacking)
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.bash_profile"]="0:0:755"
+  ["/root/auto_install.sh"]="0:0:755"
+)
